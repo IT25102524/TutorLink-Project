80bb333afde6b7a15c6dfb2b741a0043a26850a2
IT25102524 <it25102524@my.sliit.lk>
2026-05-15 11:53:28 +0530

    Initial Spring Boot project structure

---
+++ Tutorlink/src/main/java/com/tutorlink/TutorlinkApplication.java
@@ -0,0 +1,13 @@
+package com.tutorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorlinkApplication.class, args);
+	}
+
+}
---
+++ Tutorlink/src/test/java/com/tutorlink/TutorlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
